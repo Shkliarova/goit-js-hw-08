07a26446c99c14534821fabd4d4df830c4628cbb
Shkliarova <shklyarovaalisa@gmail.com>
2023-09-12 21:18:44 +0300

    does task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('loaded', function() {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+      player.setCurrentTime(parseFloat(currentTime));
+    }
+  });
+  
+  player.on('timeupdate', throttle(videoPlay, 1000));
+  
+  function videoPlay({seconds}){
+      localStorage.setItem('videoplayer-current-time', seconds);
+  }
+
+  
+  
+  
+  
+  
+  
